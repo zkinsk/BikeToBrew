14cb723721d802cf2b58bc928b60f54e49fd295b
zkinsk <zoolunk@gmail.com>
2019-12-18 21:39:39 -0500

    changes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -511,22 +511,6 @@ function panZoom(lat, lon) {
 	map.setZoom(14);
 }
 
-// distance input validation
-//  function distance() {
-// 	let d = $('#dist').val();
-// 	if (d > 50) {
-// 		d = 50;
-// 		$('#dist').val('50');
-// 	} else if (d < 0) {
-// 		d = 1;
-// 		$('#dist').val('1');
-// 	} else if (d == '') {
-// 		d = 5;
-// 		$('#dist').val('5');
-// 	}
-// 	return d;
-// };
-
 // hides the splash screen after a set amount of time then shows the app
 function splashScreen() {
 	setTimeout(function() {
